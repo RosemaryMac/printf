aa05b70b32c7f13e3bdd2b3cdd1c8df93eccb4c3
RosemaryMac <rmachanja14@gmail.com>
2022-07-10 12:49:35 -0700

    main

--- main.h
+++ main.h
@@ -1,15 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
 #include <stdarg.h>
-
 /**
  * struct print - structure for printing various types
  * @t: type to print
  * @f: function to print
  */
-
 typedef struct print
 {
 	char *t;
@@ -22,12 +19,6 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
-int print_f(va_list f);
-int print_e(va_list e);
-int print_g(va_list g);
-int print_l(va_list l);
-int print_0(va_list zero);
-int print_h(va_list h);
 int print_u(va_list u);
 int print_b(va_list b);
 int print_o(va_list o);
@@ -37,9 +28,5 @@ int print_p(va_list p);
 int print_S(va_list S);
 int print_r(va_list r);
 int print_R(va_list R);
-int print_add(va_list add);
-int print_less(va_list less);
-int print_space(va_list space);
-int print_sharp(va_list sharp);
 
 #endif
