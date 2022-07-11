643dc9ffa96b3553dcc9defeb2d2d229b3ad56af
RosemaryMac <rmachanja14@gmail.com>
2022-07-11 04:24:12 -0700

    functions_1

---
+++ arg_str_functions_1.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <stdarg.h>
+
+#include <stdlib.h>
+
+/**
+ * print_percent -  print percent character
+ * @args: char
+ * Return: percent character
+ */
+
+int print_percent(va_list args __attribute__((unused)))
+{
+return (_putchar('%'));
+}
